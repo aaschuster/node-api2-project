7cdaddfbab3f3f0590ae8341e934c288dcae14ea
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-15 17:14:29 -0400

    basic server built and running

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send("root response");
+});
+
+module.exports = server;
