d76f3e4924472f0f8c162b84196263c0c1e0efc6
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-16 15:46:41 -0400

    build out get comments functionality

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -88,4 +88,24 @@ router.delete("/:id", async (req, res) => {
     }
 })
 
+router.get("/:id/comments", async (req, res) => {
+    const {id} = req.params;
+    const post = await Posts.findById(id);
+    
+    Posts.findPostComments(id)
+        .then( comments => {
+            if(post) res.status(200).json(comments);
+            else {
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist" 
+                })
+            }
+        })
+        .catch( err => {
+            res.status(500).json({
+                message: "The comments information could not be retrieved"
+            })
+        })
+})
+
 module.exports = router;
